46c8dd6091c2e4b365a3cb016941611d9538fda1
rii922 <147631072+rii922@users.noreply.github.com>
2024-10-08 20:22:43 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
 import discord
 import os
-from dotenv import load_dotenv
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
-
-load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 BOT_NOTIFICATION_CHANNEL_ID = int(os.getenv("BOT_NOTIFICATION_CHANNEL_ID"))
